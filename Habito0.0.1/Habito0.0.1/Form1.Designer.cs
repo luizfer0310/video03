0aaf629075897c899c3f0cbac1c49566a3fde111
Administrador <Administrador@LUIZ-PC>
2017-06-08 21:06:20 -0300

    Programa em C# referente ao vídeo https://www.youtube.com/watch?v=9BtrLf6PfYY

---
+++ Habito0.0.1/Habito0.0.1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Habito0._0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTarefa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblTarefa = new System.Windows.Forms.Label();
+            this.lblSeg = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblHoras = new System.Windows.Forms.Label();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(146, 46);
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(100, 20);
+            this.txtTarefa.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tarefa:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(264, 44);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(146, 104);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(221, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 6, 7, 19, 4, 13, 0);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Agendar:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Minutos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Horas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(104, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Tarefa:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(104, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Segundos:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(104, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Dias:";
+            // 
+            // lblTarefa
+            // 
+            this.lblTarefa.AutoSize = true;
+            this.lblTarefa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTarefa.Location = new System.Drawing.Point(182, 326);
+            this.lblTarefa.Name = "lblTarefa";
+            this.lblTarefa.Size = new System.Drawing.Size(51, 16);
+            this.lblTarefa.TabIndex = 17;
+            this.lblTarefa.Text = "label8";
+            // 
+            // lblSeg
+            // 
+            this.lblSeg.AutoSize = true;
+            this.lblSeg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeg.Location = new System.Drawing.Point(182, 292);
+            this.lblSeg.Name = "lblSeg";
+            this.lblSeg.Size = new System.Drawing.Size(51, 16);
+            this.lblSeg.TabIndex = 18;
+            this.lblSeg.Text = "label8";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(182, 257);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(51, 16);
+            this.lblMin.TabIndex = 19;
+            this.lblMin.Text = "label8";
+            // 
+            // lblHoras
+            // 
+            this.lblHoras.AutoSize = true;
+            this.lblHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoras.Location = new System.Drawing.Point(182, 221);
+            this.lblHoras.Name = "lblHoras";
+            this.lblHoras.Size = new System.Drawing.Size(51, 16);
+            this.lblHoras.TabIndex = 20;
+            this.lblHoras.Text = "label8";
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDias.Location = new System.Drawing.Point(182, 190);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(51, 16);
+            this.lblDias.TabIndex = 21;
+            this.lblDias.Text = "label8";
+            this.lblDias.Click += new System.EventHandler(this.lblDias_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 423);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.lblHoras);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblSeg);
+            this.Controls.Add(this.lblTarefa);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTarefa);
+            this.Name = "Form1";
+            this.Text = "BomHabito";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTarefa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTarefa;
+        private System.Windows.Forms.Label lblSeg;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblHoras;
+        private System.Windows.Forms.Label lblDias;
+    }
+}
+
---
+++ Habito0.0.1/Habito0.0.1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Habito0._0._1
+{
+    public partial class Form1 : Form
+    {
+       // private DateTime dtFim;
+        public Form1()
+        {
+            InitializeComponent();
+
+           // dtFim = dateTimePicker1.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = dateTimePicker1.Value.AddMinutes(2);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value < DateTime.Now)
+            {
+                MessageBox.Show("Escolha uma data futura", "Erro", MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                timer1.Start();
+                lblTarefa.Text = txtTarefa.Text;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value <= DateTime.Now)
+            {
+                MessageBox.Show("Hora de executar a Tarefa", "Conclusão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Enabled = false;
+            }
+            else
+            {
+                TimeSpan tsTempoFaltante = new TimeSpan();
+                tsTempoFaltante = dateTimePicker1.Value - DateTime.Now;
+                lblDias.Text = tsTempoFaltante.Days.ToString();
+                lblHoras.Text = tsTempoFaltante.Hours.ToString();
+                lblMin.Text = tsTempoFaltante.Minutes.ToString();
+                lblSeg.Text = tsTempoFaltante.Seconds.ToString();
+
+            }
+        }
+
+        private void lblDias_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
